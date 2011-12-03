d39aa5e48524fc130492df5b6a852c5014d270a0
Alex Michael <alexandros.michael@gmail.com>
2011-12-03 14:22:48 +0000

    Knob bars (tryin to figure out)

--- app/app.js
+++ app/app.js
@@ -1,4 +1,16 @@
-$(function(){
+var Knob = function(elemId, searchTerm)
+{
+	var elemId     = elemId,
+		elem       = $("#" + elemId),
+		searchTerm = searchTerm,
+		rad2deg    = 180/Math.PI,
+		deg 	   = 0,
+		bars       = elem.find('.bars'),
+		steps      = 20,
+		height     = elem.height(),
+		width      = elem.width();
+	
+	this.boost = 0;
 	
 	var colors = [
 		'26e000','2fe300','37e700','45ea00','51ef00',
@@ -9,42 +21,58 @@ $(function(){
 		'ff5f04','ff4f03','f83a00','ee2b00','e52000'
 	];
 	
-	var rad2deg = 180/Math.PI;
-	var deg = 0;
-	var bars = $('#bars');
-	
-	/*for(var i=0;i<colors.length;i++){
+	for(var i = 0; i < steps; i++){
 		
-		deg = i*12;
+		deg = i*9;
 		
 		$('<div class="colorBar">').css({
-			backgroundColor: '#'+colors[i],
+			backgroundColor: 'white',
 			transform:'rotate('+deg+'deg)',
-			top: -Math.sin(deg/rad2deg)*80+100,
+			top: -Math.sin(deg/rad2deg)*80+30,
 			left: Math.cos((180 - deg)/rad2deg)*80+100,
 		}).appendTo(bars);
-	}*/
+	}
 	
 	var colorBars = bars.find('.colorBar');
 	var numBars = 0, lastNum = -1;
 	
-	$('#knob1').knobKnob({
+	$(elem.find(".control")).knobKnob({
 		snap : 10,
-		value: 154,
+		value: 0,
 		turn : function(ratio){
-			console.log(ratio);
-			/*
-			numBars = Math.round(colorBars.length*ratio);
-			
-			if(numBars == lastNum){
-				return false;
-			}
-			lastNum = numBars;
-			
-			colorBars.removeClass('active').slice(0, numBars).addClass('active');
-			*/
 		}
 	});
-	console.log("aafa");
 	
-});
+};
+
+
+var App = function() 
+{
+	var knobs = [];
+	
+	return {
+		
+		init: function() 
+		{
+			var h = $(window).height() - 50;	
+			$("#main").height(h);
+			
+			// Make knobs.
+			var proxied = $.proxy(function(i, e) {
+				console.log(this);
+				var id   = $(e).attr("id"),
+					term = id.split("-")[1];
+				knobs.push(new Knob(id, term));
+			}, this);
+
+			$(".synthi-knob").each(proxied);
+		},
+		
+		knobs: function() 
+		{
+			return knobs;
+		}
+	
+	};
+
+};
