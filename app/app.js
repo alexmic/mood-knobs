167c0c9688d2022224cda1bc9ed3ab2e1a01c4b8
Alex Michael <alexandros.michael@gmail.com>
2011-12-03 14:46:16 +0000

    Nicer knobs. Getting there.

--- app/app.js
+++ app/app.js
@@ -6,30 +6,24 @@ var Knob = function(elemId, searchTerm)
 		rad2deg    = 180/Math.PI,
 		deg 	   = 0,
 		bars       = elem.find('.bars'),
-		steps      = 20,
+		steps      = 10,
 		height     = elem.height(),
-		width      = elem.width();
+		width      = elem.width(),
+		radius	   = elem.find(".control").width() / 1.2;
+	
+	console.log(height);
+	console.log(width);
 	
 	this.boost = 0;
 	
-	var colors = [
-		'26e000','2fe300','37e700','45ea00','51ef00',
-		'61f800','6bfb00','77ff02','80ff05','8cff09',
-		'93ff0b','9eff09','a9ff07','c2ff03','d7ff07',
-		'f2ff0a','fff30a','ffdc09','ffce0a','ffc30a',
-		'ffb509','ffa808','ff9908','ff8607','ff7005',
-		'ff5f04','ff4f03','f83a00','ee2b00','e52000'
-	];
-	
-	for(var i = 0; i < steps; i++){
+	for(var i = 1; i < steps; i++){
 		
-		deg = i*9;
+		deg = i*18;
 		
 		$('<div class="colorBar">').css({
-			backgroundColor: 'white',
 			transform:'rotate('+deg+'deg)',
-			top: -Math.sin(deg/rad2deg)*80+30,
-			left: Math.cos((180 - deg)/rad2deg)*80+100,
+			top: -Math.sin(deg/rad2deg)*radius+65,
+			left: Math.cos((180 - deg)/rad2deg)*radius+60.5,
 		}).appendTo(bars);
 	}
 	
